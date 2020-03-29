cff46b5138f6f24bc5cd28709457b1947055884a
Xyrai <Xyraidev@gmail.com>
2020-03-30 01:01:46 +0200

    Added redirection to encryption/decryption

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+
+    @FXML
+    private void startEncryption(ActionEvent event) throws IOException {
+        Node source = (Node) event.getSource();
+        Stage dialogStage = (Stage) source.getScene().getWindow();
+        dialogStage.close();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("encryption.fxml")));
+        dialogStage.setScene(scene);
+        dialogStage.show();
+    }
+
+    @FXML
+    private void startDecryption(ActionEvent event) throws IOException {
+        Node source = (Node) event.getSource();
+        Stage dialogStage = (Stage) source.getScene().getWindow();
+        dialogStage.close();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("decryption.fxml")));
+        dialogStage.setScene(scene);
+        dialogStage.show();
+    }
 }
