28332645fd9162f228deef6db891d56a07a8d99f
Xyrai <Xyraidev@gmail.com>
2020-03-30 00:17:27 +0200

    Changed main scene styling

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("RSA");
+        primaryStage.setTitle("RSA Cryptosystem");
         primaryStage.setScene(new Scene(root, 800,600));
         primaryStage.show();
     }
